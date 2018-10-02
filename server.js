4c143d0d7a767ba72c7c7e968d314a33afe2bbc1
Jarek Kajko <jaruto7@gmail.com>
2018-10-02 16:38:27 +0200

    Fix code - in progress

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require( 'mongoose' );
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds233970.mlab.com:33970/ds119810', {
+mongoose.connect( 'mongodb://<dbuser>:<dbpassword>@ds233970.mlab.com:33970/ds119810', {
     useMongoClient: true
 });
 
@@ -17,28 +17,28 @@ const userSchema = new Schema({
 });
 
 //Mongoose schema method
-userSchema.methods.manify = function(next) {
+userSchema.methods.manify = function( next ) {
     this.name = this.name + '-boy';
 
-    return next(null, this.name);
+    return next( null, this.name );
 };
 
 //pre-save method
-userSchema.pre('save', function(next) {
+userSchema.pre ('save', function( next ) {
     //pobranie aktualnego czasu
     const currentDate = new Date();
 
     //zmiana pola na aktualny czas
     this.updated_at = currentDate;
 
-    if (!this.created_at)
+    if ( !this.created_at )
         this.created_at = currentDate;
 
     next();
 });
 
 //model based on userSchema
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model( 'User', userSchema );
 
 //instancje klasy User
 const kenny = new User({
@@ -47,9 +47,9 @@ const kenny = new User({
     password: 'password'
 });
 
-kenny.manify(function(err, name) {
-    if (err) throw err;
-    console.log('Twoje nowe imię to: ' + name);
+kenny.manify( function( err, name ) {
+    if ( err ) throw err;
+    console.log( 'Twoje nowe imię to: ' + name );
 });
 
 const benny = new User({
@@ -58,9 +58,9 @@ const benny = new User({
     password: 'password'
 });
 
-benny.manify(function(err, name) {
-    if (err) throw err;
-    console.log('Twoje nowe imię to: ' + name);
+benny.manify( function( err, name ) {
+    if ( err ) throw err;
+    console.log( 'Twoje nowe imię to: ' + name );
 });
 
 const mark = new User({
@@ -69,58 +69,58 @@ const mark = new User({
     password: 'password'
 });
 
-mark.manify(function(err, name) {
-    if (err) throw err;
-    console.log('Twoje nowe imię to: ' + name);
+mark.manify( function( err, name ) {
+    if ( err ) throw err;
+    console.log( 'Twoje nowe imię to: ' + name );
 });
 
 const findAllUsers = function() {
     // find all users
-    return User.find({}, function(err, res) {
-        if (err) throw err;
-        console.log('Actual database records are ' + res);
+    return User.find({}, function( err, res ) {
+        if ( err ) throw err;
+        console.log( 'Actual database records are ' + res );
     });
 }
 
 const findSpecificRecord = function() {
     // find specific record
-    return User.find({ username: 'Kenny_the_boy' }, function(err, res) {
-        if (err) throw err;
-        console.log('Record you are looking for is ' + res);
+    return User.find({ username: 'Kenny_the_boy' }, function( err, res ) {
+        if ( err ) throw err;
+        console.log( 'Record you are looking for is ' + res );
     })
 }
 
-const updadeUserPassword = function() {
+const updateUserPassword = function() {
     // update user password
     return User.findOne({ username: 'Kenny_the_boy' })
-        .then(function(user) {
-            console.log('Old password is ' + user.password);
-            console.log('Name ' + user.name);
+        .then(function( user ) {
+            console.log( 'Old password is ' + user.password );
+            console.log( 'Name ' + user.name );
             user.password = 'newPassword';
-            console.log('New password is ' + user.password);
-            return user.save(function(err) {
-                if (err) throw err;
+            console.log( 'New password is ' + user.password );
+            return user.save( function( err ) {
+                if ( err ) throw err;
 
-                console.log('Uzytkownik ' + user.name + ' zostal pomyslnie zaktualizowany');
+                console.log('Uzytkownik ' + user.name + ' zostal pomyslnie zaktualizowany' );
             })
         })
 }
 
-const updateUsername = function() {
+const updateUserName = function() {
     // update username
-    return User.findOneAndUpdate({ username: 'Benny_the_boy' }, { username: 'Benny_the_man' }, { new: true }, function(err, user) {
-        if (err) throw err;
+    return User.findOneAndUpdate({ username: 'Benny_the_boy' }, { username: 'Benny_the_man' }, { new: true }, function( err, user ) {
+        if ( err ) throw err;
 
-        console.log('Nazwa uzytkownika po aktualizacji to ' + user.username);
+        console.log( 'Nazwa uzytkownika po aktualizacji to ' + user.username );
     })
 }
 
 const findMarkAndDelete = function() {
     // find specific user and delete
     return User.findOne({ username: 'Mark_the_boy' })
-        .then(function(user) {
-            return user.remove(function() {
-                console.log('User successfully deleted');
+        .then( function( user ) {
+            return user.remove( function() {
+                console.log( 'User successfully deleted' );
             });
         })
 }
@@ -128,9 +128,9 @@ const findMarkAndDelete = function() {
 const findKennyAndDelete = function() {
     // find specific user and delete
     return User.findOne({ username: 'Kenny_the_boy' })
-        .then(function(user) {
-            return user.remove(function() {
-                console.log('User successfully deleted');
+        .then(function( user ) {
+            return user.remove( function() {
+                console.log( 'User successfully deleted' );
             });
         });
 }
@@ -138,19 +138,19 @@ const findKennyAndDelete = function() {
 const findBennyAndRemove = function() {
     // find specific user and delete
     return User.findOneAndRemove({ username: 'Benny_the_man' })
-        .then(function(user) {
-            return user.remove(function() {
-                console.log('User successfully deleted');
+        .then(function( user ) {
+            return user.remove( function() {
+                console.log( 'User successfully deleted' );
             });
         });
 }
 
-Promise.all([kenny.save(), mark.save(), benny.save()])
-    .then(findAllUsers)
-    .then(findSpecificRecord)
-    .then(updadeUserPassword)
-    .then(updateUsername)
-    .then(findMarkAndDelete)
-    .then(findKennyAndDelete)
-    .then(findBennyAndRemove)
-    .catch(console.log.bind(console))
+Promise.all( [ kenny.save(), mark.save(), benny.save() ] )
+    .then( findAllUsers )
+    .then( findSpecificRecord )
+    .then( updateUserPassword )
+    .then( updateUserName )
+    .then( findMarkAndDelete )
+    .then( findKennyAndDelete )
+    .then( findBennyAndRemove )
+    .catch( console.log.bind( console ) )
